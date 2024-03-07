a31e59c74dfad07fb6350906e08ca7c2e0b38af3
Nikita Kornuk <kornyk62@gmail.com>
2024-03-07 22:37:46 +0100

    add project

---
+++ 8March/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8March
+//
+//  Created by NikitaKorniuk   on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
